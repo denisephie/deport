311ab8021daa511a6c3df399370f37494d7f6780
denisephie <denisephie@gmail.com>
2026-08-17 01:53:22 +0800

    feat: data-time attribute to follow system clock

--- src/context/TimeOfDay.jsx
+++ src/context/TimeOfDay.jsx
@@ -1,8 +1,23 @@
-export function getTimeofDay(hour) {
+import { useEffect } from "react"
+
+export function getTimeofDay(hour) { /* export so oth modules can import it */
     if (hour >= 5 && hour < 11) return "morning";
     if (hour >= 11 && hour < 16) return "afternoon";
     if (hour >= 16 && hour < 19) return "evening";
     if (hour >= 19 || hour < 5) return "night";
 }
 
-getTimeofDay(4)
+/* define component TimeOfDayProvider */
+/* destructure using { children } 
+unpack the key of properties in children
+for it to be called by its own name */
+
+export function TimeOfDayProvider({ children }) {
+    useEffect(() => { /* where the side effects go */
+        const bucket = getTimeofDay(new Date().getHours())
+        /* new Date() makes Date object for right now */
+        document.documentElement.setAttribute("data-time", bucket) /* up to Element -> html, setAttribute adds data-time = night to it */
+    }, []) /* dependency array, run once after the first (NOT EVERY -> infinite loop) render */
+
+    return children
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { TimeOfDayProvider } from './context/TimeOfDay.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( /* render opens JSX position */
   <StrictMode>
-    <App />
+        <TimeOfDayProvider>
+           <App />
+        </TimeOfDayProvider>
   </StrictMode>,
 )
